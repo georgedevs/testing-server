ee8cd216dec445e1d41598dc0f5c2145cc87fb07
georgedevs <gukohgodwin@gmail.com>
2025-01-18 02:06:36 +0100

    auth update

--- build/utils/jwt.js
+++ build/utils/jwt.js
@@ -11,7 +11,7 @@ exports.refreshTokenOptions = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: 'strict',
+    sameSite: 'none',
     secure: true,
 };
 // Token generation functions remain similar
--- utils/jwt.ts
+++ utils/jwt.ts
@@ -26,7 +26,7 @@ export const refreshTokenOptions: ITokenOptions = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: 'strict',
+    sameSite: 'none',
     secure: true,
 };
 
